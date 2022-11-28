7a0856a3b409a676c9d87f3ee5287f5d9408bd62
wugp <17674101779@163.com>
2022-11-28 15:13:16 +0800

    fix README.md

--- bot/bot_file.go
+++ bot/bot_file.go
@@ -18,7 +18,6 @@ func download(fileId string) (tgbotapi.File, error) {
 
 // 重命名
 func rename(FilePath, FileName string) string {
-	FilePath = strings.Replace(FilePath, "/root/", "/home/wuguipeng/", 1)
 	split := strings.Split(FilePath, "/")
 	split[len(split)-1] = FileName
 	join := strings.Join(split, "/")
